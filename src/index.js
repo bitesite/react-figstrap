777255585e010bc14e1c4bcf8a109b18d01bc9ab
Casey Li <casey.li@gmail.com>
2022-09-26 15:05:06 -0400

    Added default box-sizing: border-box.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Frame = styled.div`
   display: flex;
-  flex-direction: ${(props) => (props.vertical ? "column" : "row")};
+  ${(props) => {
+    if (props.vertical) {
+      return css`
+        flex-direction: column;
+      `;
+    }
+  }}
+  box-sizing: border-box;
   gap: ${(props) => `${props.gap ? props.gap : "0"}px`};
   padding: ${(props) => `${props.padding ? props.padding : "0"}px`};
   align-items: ${(props) =>
